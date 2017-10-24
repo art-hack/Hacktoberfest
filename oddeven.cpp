fe0f1c80dbe7abe94d39ecf1a7fbda8c9cb0c2aa
amo2123 <amogha218@gmail.com>
2017-10-24 20:38:07 +0530

    Made the code more readable

--- oddeven.cpp
+++ oddeven.cpp
@@ -1,9 +1,21 @@
+// Program to test whether  the given input is even or odd 
 #include <iostream>
 using namespace std;
 
+
 int main() {
-	int n;
-	cin >> n;
-	if (n % 2) cout << "ODD\n";
-	else cout << "EVEN\n";
+	
+	int inputNumber;
+	
+	cout << "\n Enter a number to be tested : " ;
+	cin >> inputNumber ;
+	
+	if (n % 2) {
+		cout << " It is an Odd Value \n " ;
+	}
+	else {
+		cout << " It is an even Value \n " ;
+	}
+	
+	return 0;
 }
